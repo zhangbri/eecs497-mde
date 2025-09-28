0d077de8306d2c9887c848a0a659f11d5df3533d
Brian Zhang <zhangbri@umich.edu>
2025-09-28 19:51:42 -0400

    iOS Simulator Setup

---
+++ ScreenTime/ScreenTime/ScreenTimeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScreenTimeApp.swift
+//  ScreenTime
+//
+//  Created by Brian Zhang on 9/28/25.
+//
+
+import SwiftUI
+
+@main
+struct ScreenTimeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
